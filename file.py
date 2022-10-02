412597686a899a462c550e9483bee2467af85b0c
yuyu <hcsd7b34@gmail.com>
2022-10-02 13:55:01 +0900

    Fix: 添付ファイル付きのファイルが開けない

--- file.py
+++ file.py
@@ -10,8 +10,8 @@ def save(tftr_data: TftrData, path: str):
   data += b'\x03' + int(tftr_data.edit_deadline.timestamp()).to_bytes(8, 'big')
   content_bytes = tftr_data.content.encode('utf-8')
   data += b'\x04' + len(content_bytes).to_bytes(8, 'big') + content_bytes
-  data += b'\x05' + len(tftr_data.files.keys()).to_bytes(2, 'big') + \
-    reduce(lambda a, b: a + b, [len(k.encode('utf-8')).to_bytes(2, 'big') + len(v).to_bytes(8, 'big') + k.encode('utf-8') + v for k, v in tftr_data.files.items()], b'')
+  data += b'\x05' + len(tftr_data.attachments.keys()).to_bytes(2, 'big') + \
+    reduce(lambda a, b: a + b, [len(k.encode('utf-8')).to_bytes(2, 'big') + len(v).to_bytes(8, 'big') + k.encode('utf-8') + v for k, v in tftr_data.attachments.items()], b'')
   
   f = open(path, 'wb')
   f.write(data)
@@ -53,19 +53,19 @@ def load(path: str = ''):
         file_count = int.from_bytes(data[i+1:i+3], 'big')
         i += 3
 
-        files: dict[str, bytes] = {}
-        for i in range(file_count):
-          file_name_len = data[i:i+2]
+        attachments: dict[str, bytes] = {}
+        for j in range(file_count):
+          file_name_len = int.from_bytes(data[i:i+2], 'big')
           i += 2
-          file_data_len = data[i:i+8]
+          file_data_len = int.from_bytes(data[i:i+8], 'big')
           i += 8
           file_name = data[i:i+file_name_len].decode('utf-8')
           i += file_name_len
           file_data = data[i:i+file_data_len]
           i += file_data_len
-          files[file_name] = file_data
+          attachments[file_name] = file_data
         
-        tftr_data.files = files
+        tftr_data.attachments = attachments
       case _:
         return None
   
