188783371a9ef78408389c574fe394548549b941
yuyu <hcsd7b34@gmail.com>
2022-10-02 14:04:36 +0900

    Fix: ショートカットキーが使えない

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def open_file(event = None):
           {'add_attachment': add_attachment, 'delete_attachment': delete_attachment, 'rename_attachment': rename_attachment, 'save_attachment': save_attachment, 'open_attachment': open_attachment})
       elif datetime.now() >= tftr_data.viewable_date:
         state = State.VIEW
-        update(state, tftr_data, root)
+        update(state, tftr_data, root, {'save_attachment': save_attachment, 'open_attachment': open_attachment})
       else:
         viewable_date = tftr_data.viewable_date.strftime("%Y/%m/%y %H:%M")
         messagebox.showinfo(title='閲覧不可', message=f'このファイルは{viewable_date}まで閲覧できません')
@@ -139,10 +139,10 @@ menu_file.add_command(label='新規作成', accelerator='Ctrl+N', command=create
 menu_file.add_command(label='開く', accelerator='Ctrl+O', command=open_file)
 menu_file.add_command(label='上書き保存', accelerator='Ctrl+S', command=save)
 menu_file.add_command(label='名前をつけて保存', accelerator='Ctrl+Shift+S', command=save_as)
-menu_file.bind_all('<Control-n>', create_new)
-menu_file.bind_all('<Control-o>', open_file)
-menu_file.bind_all('<Control-s>', save)
-menu_file.bind_all('<Control-Shift-S>', save_as)
+root.bind_all('<Control-n>', create_new)
+root.bind_all('<Control-o>', open_file)
+root.bind_all('<Control-s>', save)
+root.bind_all('<Control-Shift-S>', save_as)
 
 menubar.add_cascade(label='ファイル', menu=menu_file)
 root.config(menu=menubar)
